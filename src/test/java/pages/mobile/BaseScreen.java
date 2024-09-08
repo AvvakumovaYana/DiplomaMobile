3ae3874391095db4368e4f80bc7f7c5797a77399
Yana Avvakumova <10.07.93t@gmail.com>
2024-09-08 13:27:44 +0300

    Refactoring

--- src/test/java/pages/mobile/BaseScreen.java
+++ src/test/java/pages/mobile/BaseScreen.java
@@ -3,15 +3,11 @@ package pages.mobile;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 
-import static com.codeborne.selenide.Selenide.sleep;
+import java.time.Duration;
 
 public class BaseScreen {
 
-    protected static void ensureElement(SelenideElement element, int waitingPeriodInSeconds) throws InterruptedException {
-        while (!(element.is(Condition.exist) && element.isDisplayed()) && waitingPeriodInSeconds > 0) {
-            sleep(1000);
-            waitingPeriodInSeconds--;
-            System.out.println("Wait for " + waitingPeriodInSeconds);
-        }
+    protected static void ensureElement(SelenideElement element, int waitingPeriodInSeconds) {
+        element.shouldBe(Condition.visible, Duration.ofSeconds(waitingPeriodInSeconds));
     }
 }
